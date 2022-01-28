d070043336d2506429e38931602b9d17443406fb
Dmitriy <jeoffdota@gmail.com>
2022-01-28 23:45:54 +0200

    1.05 v20

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class menu : MonoBehaviour
@@ -9,6 +10,7 @@ public class menu : MonoBehaviour
     [SerializeField] private String privacyPolicyLink = "https://telegra.ph/Privacy-Policy-01-02-36";
     [SerializeField] private String termsOfUseLink = "https://telegra.ph/Terms--Conditions-01-25";
     [SerializeField] private String googlePlayLink = "https://play.google.com/store/apps/details?id=com.tapmushrooms";
+    [SerializeField] private TMP_Text textQuality;
     public void btnRayeUs()
     {
         Application.OpenURL(googlePlayLink);
@@ -22,13 +24,16 @@ public class menu : MonoBehaviour
 
     public void Set0q()
     {
+        textQuality.text = QualitySettings.GetQualityLevel() + " quality";
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings before");
         QualitySettings.SetQualityLevel(0);
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings after");
+        
     }
     
     public void Set1q()
     {
+        textQuality.text = QualitySettings.GetQualityLevel() + " quality";
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings before");
         QualitySettings.SetQualityLevel(1);
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings after");
@@ -36,6 +41,7 @@ public class menu : MonoBehaviour
     
     public void Set2q()
     {
+        textQuality.text = QualitySettings.GetQualityLevel() + " quality";
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings before");
         QualitySettings.SetQualityLevel(2);
         Debug.Log(  QualitySettings.GetQualityLevel() +  "  QualitySettings after");
