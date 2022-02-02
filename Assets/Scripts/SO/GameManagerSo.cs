af0edbbf81b3e0359e8b6128946fcad4a5c85d05
Dmitriy <jeoffdota@gmail.com>
2022-02-02 13:13:59 +0200

    Revert "Revert "Revert "Revert "demo hand v3""""
    This reverts commit c83a492c28564cd3a1f0633f016020a27ae19db2.

--- Assets/Scripts/SO/GameManagerSo.cs
+++ Assets/Scripts/SO/GameManagerSo.cs
@@ -33,7 +33,7 @@ public class GameManagerSo : ScriptableObject
         Multiplier = 1;
         MushLifeTime = 1f;
         BombMushroomsLive = 0;
-        defaultTimeBetweenSpawn = 0.75f;
+        defaultTimeBetweenSpawn = 0.35f;
         TimeBetweenSpawn = defaultTimeBetweenSpawn;
         DiffIndex = 0;
 
